2ac1648800081665122c3295a3fd990a45dce6a0
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-01-25 14:24:45 +0530

    Signing up users and added unique index on email field

--- workspace/05-shell-to-driver-project/backend/routes/auth.js
+++ workspace/05-shell-to-driver-project/backend/routes/auth.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router;
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const db = require('../db');
 
 const router = Router();
 
@@ -15,9 +16,7 @@ router.post('/login', (req, res, next) => {
   // If yes, create token and return it to client
   const token = createToken();
   // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res
-    .status(401)
-    .json({ message: 'Authentication failed, invalid username or password.' });
+  res.status(401).json({ message: 'Authentication failed, invalid username or password.' });
 });
 
 router.post('/signup', (req, res, next) => {
@@ -26,15 +25,22 @@ router.post('/signup', (req, res, next) => {
   // Hash password before storing it in database => Encryption at Rest
   bcrypt
     .hash(pw, 12)
-    .then(hashedPW => {
+    .then((hashedPW) => {
       // Store hashedPW in database
-      console.log(hashedPW);
-      const token = createToken();
-      res
-        .status(201)
-        .json({ token: token, user: { email: 'dummy@dummy.com' } });
+      db.getDb()
+        .db()
+        .collection('users')
+        .insertOne({ email: email, password: hashedPW })
+        .then((result) => {
+          const token = createToken();
+          res.status(201).json({ token: token, user: { email: email } });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({ message: 'Creating the user failed.' });
+        });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json({ message: 'Creating the user failed.' });
     });
