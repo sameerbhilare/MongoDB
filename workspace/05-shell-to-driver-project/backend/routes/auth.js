b1504adc01377e092ff781a3c7bab3bf1eea6bf1
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-01-25 14:36:08 +0530

    sign in users

--- workspace/05-shell-to-driver-project/backend/routes/auth.js
+++ workspace/05-shell-to-driver-project/backend/routes/auth.js
@@ -14,9 +14,28 @@ router.post('/login', (req, res, next) => {
   const pw = req.body.password;
   // Check if user login is valid
   // If yes, create token and return it to client
-  const token = createToken();
-  // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res.status(401).json({ message: 'Authentication failed, invalid username or password.' });
+  db.getDb()
+    .db()
+    .collection('users')
+    .findOne({ email: email })
+    .then((userDoc) => {
+      return bcrypt.compare(pw, userDoc.password);
+    })
+    .then((isPwdMatched) => {
+      if (isPwdMatched) {
+        const token = createToken();
+        res.status(200).json({
+          message: 'Authentication succeeded.',
+          token: token,
+          user: { email: email },
+        });
+      } else {
+        throw Error();
+      }
+    })
+    .catch((err) => {
+      res.status(401).json({ message: 'Authentication failed, invalid username or password.' });
+    });
 });
 
 router.post('/signup', (req, res, next) => {
